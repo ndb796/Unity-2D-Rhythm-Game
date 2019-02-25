b363ea2f1bee1868bf21faab6a5f3f76b77b69e8
ndb796 <ndb796@naver.com>
2019-02-26 08:00:28 +0900

    충돌 감지 및 노트 판정 이벤트 처리

--- Unity 2D Rhythm Game/Assets/Scripts/GameManager.cs
+++ Unity 2D Rhythm Game/Assets/Scripts/GameManager.cs
@@ -14,6 +14,8 @@ public class GameManager : MonoBehaviour
 
     public float noteSpeed;
 
+    public enum judges { NONE = 0, BAD, GOOD, PERFECT, MISS };
+
     void Start()
     {
         
--- Unity 2D Rhythm Game/Assets/Scripts/NoteBehavior.cs
+++ Unity 2D Rhythm Game/Assets/Scripts/NoteBehavior.cs
@@ -5,14 +5,50 @@ using UnityEngine;
 public class NoteBehavior : MonoBehaviour
 {
     public int noteType;
+    private GameManager.judges judge;
+    private KeyCode keyCode;
 
     void Start()
     {
-        
+        if (noteType == 1) keyCode = KeyCode.D;
+        else if (noteType == 2) keyCode = KeyCode.F;
+        else if (noteType == 3) keyCode = KeyCode.J;
+        else if (noteType == 4) keyCode = KeyCode.K;
     }
     
     void Update()
     {
         transform.Translate(Vector3.down * GameManager.instance.noteSpeed);
+        // 사용자가 노트 키를 입력한 경우
+        if(Input.GetKey(keyCode))
+        {
+            // 해당 노트에 대한 판정을 진행합니다.
+            Debug.Log(judge);
+            // 노트가 판정 선에 닿기 시작한 이후로는 해당 노트를 제거합니다.
+            if (judge != GameManager.judges.NONE) Destroy(gameObject);
+        }
     }
+
+    // 각 노트의 현재 위치에 대하여 판정을 수행합니다.
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Bad Line")
+        {
+            judge = GameManager.judges.BAD;
+        }
+        else if(other.gameObject.tag == "Good Line")
+        {
+            judge = GameManager.judges.GOOD;
+        }
+        else if (other.gameObject.tag == "Perfect Line")
+        {
+            judge = GameManager.judges.PERFECT;
+        }
+        else if (other.gameObject.tag == "Miss Line")
+        {
+            judge = GameManager.judges.MISS;
+            Destroy(gameObject);
+        }
+    }
+
 }
