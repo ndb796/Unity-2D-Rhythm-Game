90382bbf8632d2e52ac9e8649fd99c8854f51241
ndb796 <ndb796@naver.com>
2019-02-26 07:19:37 +0900

    구분선 작업 & 노트 오브젝트 생성 및 이동

---
+++ Unity 2D Rhythm Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // GameManager를 싱글 톤 처리합니다.
+    public static GameManager instance { get; set; }
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+    }
+
+    public float noteSpeed;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity 2D Rhythm Game/Assets/Scripts/NoteBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBehavior : MonoBehaviour
+{
+    public int noteType;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        transform.Translate(Vector3.down * GameManager.instance.noteSpeed);
+    }
+}
