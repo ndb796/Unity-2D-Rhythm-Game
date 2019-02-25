56cb3321adbb094d83fef2ef045bb74c9c3689cb
ndb796 <ndb796@naver.com>
2019-02-26 08:24:10 +0900

    노트 프리팹 생성

---
+++ Unity 2D Rhythm Game/Assets/Scripts/NoteController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteController : MonoBehaviour
+{
+    // 하나의 노트에 대한 정보를 담는 노트(Note) 클래스를 정의
+    class Note
+    {
+        public int noteType { get; set; }
+        public int order { get; set; }
+        public Note(int noteType, int order)
+        {
+            this.noteType = noteType;
+            this.order = order;
+        }
+    }
+
+    public GameObject[] Notes;
+    private List<Note> notes = new List<Note>();
+    private float beatInterval = 1.0f;
+
+    IEnumerator AwaitMakeNote(Note note)
+    {
+        int noteType = note.noteType;
+        int order = note.order;
+        yield return new WaitForSeconds(order * beatInterval);
+        Instantiate(Notes[noteType - 1]);
+    }
+
+    void Start()
+    {
+        notes.Add(new Note(1, 1));
+        notes.Add(new Note(2, 2));
+        notes.Add(new Note(3, 3));
+        notes.Add(new Note(4, 4));
+        notes.Add(new Note(1, 5));
+        notes.Add(new Note(2, 6));
+        notes.Add(new Note(3, 7));
+        notes.Add(new Note(4, 8));
+        // 모든 노트를 정해진 시간에 출발하도록 설정
+        for(int i = 0; i < notes.Count; i++)
+        {
+            StartCoroutine(AwaitMakeNote(notes[i]));
+        }
+    }
+    
+    void Update()
+    {
+        
+    }
+}
