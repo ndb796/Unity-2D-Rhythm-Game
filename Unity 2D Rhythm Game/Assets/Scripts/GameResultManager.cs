1c4b88d99694b008d8ee58e15162ec7587107e81
ndb796 <ndb796@naver.com>
2019-03-10 21:31:49 +0900

    랭크 보여주기 및 게임 시작 화면으로 돌아가기

--- Unity 2D Rhythm Game/Assets/Scripts/GameResultManager.cs
+++ Unity 2D Rhythm Game/Assets/Scripts/GameResultManager.cs
@@ -2,22 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System;
+using UnityEngine.SceneManagement;
 
 public class GameResultManager : MonoBehaviour
 {
     public Text musicTitleUI;
     public Text scoreUI;
     public Text maxComboUI;
+    public Image RankUI;
 
     void Start()
     {
         musicTitleUI.text = PlayerInformation.musicTitle;
-        scoreUI.text = "" + PlayerInformation.score;
-        maxComboUI.text = "" + PlayerInformation.maxCombo;
+        scoreUI.text = "점수: " + (int) PlayerInformation.score;
+        maxComboUI.text = "최대 콤보: " + PlayerInformation.maxCombo;
+        // 리소스에서 비트(Beat) 텍스트 파일을 불러옵니다.
+        TextAsset textAsset = Resources.Load<TextAsset>("Beats/" + PlayerInformation.selectedMusic);
+        StringReader reader = new StringReader(textAsset.text);
+        // 첫 번째 줄과 두 번째 줄을 무시합니다.
+        reader.ReadLine();
+        reader.ReadLine();
+        // 세 번째 줄에 적힌 비트 정보(S 랭크 점수, A 랭크 점수, B 랭크 점수)를 읽습니다.
+        string beatInformation = reader.ReadLine();
+        int scoreS = Convert.ToInt32(beatInformation.Split(' ')[3]);
+        int scoreA = Convert.ToInt32(beatInformation.Split(' ')[4]);
+        int scoreB = Convert.ToInt32(beatInformation.Split(' ')[5]);
+        // 성적에 맞는 랭크 이미지를 불러옵니다.
+        if (PlayerInformation.score >= scoreS)
+        {
+            RankUI.sprite = Resources.Load<Sprite>("Sprites/Rank S");
+        }
+        else if(PlayerInformation.score >= scoreA)
+        {
+            RankUI.sprite = Resources.Load<Sprite>("Sprites/Rank A");
+        }
+        else if (PlayerInformation.score >= scoreB)
+        {
+            RankUI.sprite = Resources.Load<Sprite>("Sprites/Rank B");
+        }
+        else
+        {
+            RankUI.sprite = Resources.Load<Sprite>("Sprites/Rank C");
+        }
     }
     
-    void Update()
+    public void Replay()
     {
-        
+        SceneManager.LoadScene("SongSelectScene");
     }
 }
